3492a4c1e0a201e53dde3f40673c273dade1368f
Tomas Franzén <tomas@tomasf.se>
2025-01-04 14:53:22 +0100

    Add ZipArchive<URL>.Mode

--- Sources/Zip/ZipArchive+OnDisk.swift
+++ Sources/Zip/ZipArchive+OnDisk.swift
@@ -7,15 +7,20 @@ public extension ZipArchive<URL> {
     /// If the file does not exist, a new archive is created at the specified location.
     ///
     /// - Parameter fileURL: The file URL of the zip archive to be read or created.
+    /// - Parameter mode: The mode to use. The default, `Mode.readWrite`, lets you read an archive and, optionally, add new files to it. Use `Mode.overwrite` to create a new empty archive and overwrite an existing file if it exists.
     /// - Throws: An error if the initialization fails, such as if the file cannot be read or written.
-    convenience init(url fileURL: URL) throws {
+    convenience init(url fileURL: URL, mode: Mode = .readWrite) throws {
         self.init(archive: .init())
         try fileURL.withUnsafeFileSystemRepresentation { path in
-            do {
-                try get { mz_zip_reader_init_file(&$0, path, mz_uint32(MZ_ZIP_FLAG_WRITE_ALLOW_READING.rawValue)) }
-                try get { mz_zip_writer_init_from_reader_v2(&$0, path, 0) }
-            } catch ZipError.fileOpenFailed {
-                try get { mz_zip_writer_init_file_v2(&$0, path, 0, mz_uint32(MZ_ZIP_FLAG_WRITE_ALLOW_READING.rawValue)) }
+            if mode == .readWrite {
+                do {
+                    try get { mz_zip_reader_init_file(&$0, path, mz_uint32(MZ_ZIP_FLAG_WRITE_ALLOW_READING.rawValue)) }
+                    try get { mz_zip_writer_init_from_reader_v2(&$0, path, 0) }
+                } catch ZipError.fileOpenFailed {
+                    try get { mz_zip_writer_init_file_v2(&$0, path, 0, mz_uint32(MZ_ZIP_FLAG_WRITE_ALLOW_READING.rawValue)) }
+                }
+            } else {
+                try get { mz_zip_writer_init_file_v2(&$0, path, 0, 0) }
             }
         }
     }
@@ -29,5 +34,10 @@ public extension ZipArchive<URL> {
     func finalize() throws {
         try get { mz_zip_writer_finalize_archive(&$0) }
         mz_zip_writer_end(&archive)
+    }
+
+    enum Mode {
+        case readWrite
+        case overwrite
     }
 }
